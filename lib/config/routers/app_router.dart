e3c9dc7579c86fbf3606d23ced98de209510907e
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-20 13:16:09 -0500

    defined route

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../presentation/screens/screens.dart';
+import '../../features/auth/presentation/screens/screens.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
