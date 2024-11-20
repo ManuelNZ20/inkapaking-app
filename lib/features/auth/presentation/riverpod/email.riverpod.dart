aa1acaaec81db8e092f4aad9caa969698ce9247c
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-20 13:18:32 -0500

    clean code

--- lib/features/auth/presentation/riverpod/email.riverpod.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:inkapaking_app/domain/entities/email.dart';
-import 'package:inkapaking_app/infrastructure/datasources/isar_datasource.dart';
-
-final emailsProvider = FutureProvider<List<Email>>((ref) async {
-  final emailIsar = IsarDatasource();
-
-  return await emailIsar.loadEmails();
-});
