f58b914be93e55bac476292b0934fbf76dee0453
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-20 13:50:32 -0500

    remove email isar

--- lib/features/auth/presentation/riverpod/email.riverpod.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../domain/entities/email.dart';
-import '../../infrastructure/datasources/isar_datasource.dart';
-
-// final emailsProvider = FutureProvider<List<Email>>((ref) async {
-//   final emailIsar = IsarDatasource();
-
-//   return await emailIsar.loadEmails();
-// });
