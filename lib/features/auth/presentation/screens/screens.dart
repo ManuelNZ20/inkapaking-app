fa7091265ade6efa4561f22d6413e74ed91df3b1
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-20 22:04:42 -0500

    adding assets, core/constants, core/widgets and features

---
+++ lib/features/auth/presentation/screens/screens.dart
@@ -0,0 +1,6 @@
+export 'auth/login_screen.dart';
+export 'auth/register_screen.dart';
+export 'auth/confirm_register_email_screen.dart';
+export 'auth/recover_password_screen.dart';
+
+export 'home/home_screen.dart';
