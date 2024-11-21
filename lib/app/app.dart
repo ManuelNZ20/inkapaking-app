4aefe68c7ed192a6480cca15fac6ccf3539d2170
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-20 22:30:02 -0500

    clear code app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../config/config.dart';
+import '../core/core.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
