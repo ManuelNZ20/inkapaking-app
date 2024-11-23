d9d231154b731f4e6cdda829ca8d258b5ffd95f3
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-11-23 10:45:38 -0500

    init environment

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,2 +1,4 @@
 export 'routers/app_router.dart';
 export 'theme/app_theme.dart';
+export 'theme/styles.dart';
+export 'env/environment.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inkapaking_app/app/app.dart';
 
+import 'config/config.dart';
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Environment.initEnv();
+
   runApp(
     const ProviderScope(
       child: MyApp(),
